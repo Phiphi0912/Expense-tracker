c2460ce32bb94df15c3284c7650cb5a7002d1a00
phiphi0912 <610733005@gms.ndhu.edu.tw>
2022-01-12 14:52:09 +0800

    modify recordSeeder by promise.all

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,5 +1,9 @@
 const categoryData = require('./category.json')
 const Category = require('../category')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,4 +1,8 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const db = require('../../config/mongoose')
+
 const User = require('../users')
 const Category = require('../category')
 const records = require('./records.json')
@@ -23,7 +27,7 @@ db.once('open', () => {
       })
   })
 
-  seed_users.forEach(async (user) => {
+  Promise.all(seed_users.map(async (user) => {
     try {
       const hashPassword = await bcrypt.genSalt(10).then(salt => bcrypt.hash(user.password, salt))
       const createdUser = await User.create({ name: user.name, email: user.email, password: hashPassword })
@@ -32,40 +36,9 @@ db.once('open', () => {
       await Record.create(records)
 
       console.log('record done')
-      process.exit()
-
     } catch (err) {
       console.log(err)
     }
-  })
-
-  // Promise.all(
-  //   Array.from(seed_users, async (user) => {
-  //     try {
-  //       await bcrypt
-  //         .genSalt(10)
-  //         .then(salt => bcrypt.hash(user.password, salt))
-  //         .then(hash =>
-  //           User.create({
-  //             name: user.name,
-  //             email: user.email,
-  //             password: hash,
-  //           }))
-  //         .then(async createdUser => {
-  //           try {
-  //             const userId = createdUser._id
-  //             records.forEach(item => item.userId = userId)
-  //             await Record.create(records)
-  //           } catch (err) { console.log(err) }
-  //         })
-  //     } catch (err) {
-  //       console.log(err)
-  //     }
-  //   })
-  // )
-  //   .then(() => {
-  //     console.log('record done')
-  //     process.exit()
-  //   })
-  //   .catch(err => console.log(err))
+  }))
+    .finally(() => process.exit())
 })
